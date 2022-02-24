a07652490a007021cbf91753e41f2f6db53f2629
Smrivkin2 <stephanie.rivkin@kellogg.northwestern.edu>
2022-02-24 03:30:28 +0000

    End of class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,10 @@ class SessionsController < ApplicationController
         # find the user trying to login
         @user = User.where({ email: params["email"]})[0]
         if @user
-            if @user.password == params["password"]
+            if BCrypt::Password.new(@user.password) == params["password"]
+                # cookies: {}
+                # session: {}
+                session["user_id"] = @user.id
                 flash[:notice] = "You logged in!"
                 redirect_to "/companies"
             else
@@ -19,5 +22,11 @@ class SessionsController < ApplicationController
             flash[:notice] = "Nope!"
             redirect_to "/sessions/new"
         end
+    end
+
+    def destroy
+        session["user_id"] = nil
+        flash[:notice] = "Goodbye."
+        redirect_to "/sessions/new"
     end
 end
